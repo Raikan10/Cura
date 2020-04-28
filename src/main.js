e4c6ca2b453f7fff16a009c96b33f89c5bb379dd
Ajay Rajnikanth <ajay.rajnikanth@gmail.com>
2020-04-28 22:05:17 +0530

    A few changes

--- src/main.js
+++ src/main.js
@@ -16,7 +16,16 @@ Vue.config.productionTip = false;
 Vue.config.ignoredElements = [/^ion-/];
 
 //Config Comes here
-
+const FBCONFIG = {
+  apiKey: "AIzaSyArzQjmxUUZjd56ry9L0MBu3Ze3feAGmg8",
+  authDomain: "cura-fa109.firebaseapp.com",
+  databaseURL: "https://cura-fa109.firebaseio.com",
+  projectId: "cura-fa109",
+  storageBucket: "cura-fa109.appspot.com",
+  messagingSenderId: "348628641539",
+  appId: "1:348628641539:web:658da7a7791a8ebfd8ab36",
+  measurementId: "G-786BXHTQJ6"
+};
 Vue.use(VueFirebase, {firebase: firebase, config: FBCONFIG});
 
 new Vue({
